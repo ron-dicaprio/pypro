20874095d416c7a6a8e78589245b1a1c75f252a0
ron-dicaprio <love6018@hotmail.com>
2022-10-02 14:59:40 +0800

    Update pexels_pic.py

--- pexels_pic.py
+++ pexels_pic.py
@@ -1,10 +1,13 @@
-# main website https://www.pexels.com
-# how to download pictures in pexels
+'''
+main website https://www.pexels.com
+how to download pictures in pexels
+'''
 
 import requests
 
 weburl ='https://www.pexels.com/zh-cn/api/v2/feed?seed=2022-10-02T14:39:12.839Z&per_page=30'
 
+# headers should have secret-key
 chrome_header = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
 'sec-ch-ua':'"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
 'secret-key':'H2jk9uKnhRmL6WPwh89zBezWvr'
