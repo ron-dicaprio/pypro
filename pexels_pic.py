c68b7825c124e5d2b72965a46e8be7905f7eb079
ron-dicaprio <love6018@hotmail.com>
2022-10-03 15:12:32 +0800

    Update pexels_pic.py

--- pexels_pic.py
+++ pexels_pic.py
@@ -6,15 +6,16 @@ how to download pictures in pexels
 #-*- coding:utf-8 -*-
 import requests, re
 
-# get filename from url
+# get filename from url address.
 def get_image_name(url):
     pattern = re.search("([|.|\w|\s|-])*?.(jpg|gif|png|jpeg)", url)
     if not pattern:
-        return
+        return 0
     image_name = pattern.group()
     return image_name
 
-weburl ='https://www.pexels.com/zh-cn/api/v2/feed?seed=2022-10-02T14:39:12.839Z&per_page=30'
+# default weburl
+weburl ='https://www.pexels.com/zh-cn/api/v2/feed?seed=2022-10-03T14:39:12.839Z&per_page=8'
 
 # secret-key should in headers
 chrome_header = {
@@ -23,17 +24,31 @@ chrome_header = {
     'secret-key':'H2jk9uKnhRmL6WPwh89zBezWvr'
     }
 
+# allow_redirects允许跳转
 response =requests.get(weburl, headers=chrome_header,allow_redirects=True)
 
 re_file = re.findall(r'"download_link":"(.*?)"' ,str(response.text))
 
-#print(type(re_file))
-for i in re_file:
-    
-    pic =i.split('?')[0]
-    if '.jpeg' in i:
-        print(pic)
-        ress = requests.get(pic,headers=chrome_header)
-        with open(get_image_name(pic),'ab+') as file:
-            file.write(ress.content)
-            exit(-1)
+for fileurl in re_file:
+    if '.jpeg' in fileurl:
+        download_link = fileurl.split('?')[0]
+        with open(get_image_name(download_link),'ab+') as files:
+            res = requests.get(download_link,headers=chrome_header)
+            files.write(res.content)
+
+            
+# 利用阿里云盘cli客户端上传
+import os
+filepath = os.getcwd()
+for file in os.listdir(filepath):
+    print(file)
+    if '.jpeg' in file:
+        try:
+            # system级别调用cmd
+            os.system('/home/ubuntu/aliyunpan-v0.2.2-linux-amd64/aliyunpan upload %s /Public_share' % (file))
+        except Exception as e:
+            print(e)
+
+
+
+            
