6dbcbfa040fb004b9f0b5d5329f27d95e12d1d0e
ron-dicaprio <love6018@hotmail.com>
2022-10-10 11:35:33 +0800

    Update logfile.py

--- logfile.py
+++ logfile.py
@@ -6,24 +6,29 @@ the reason why i want to write this silly function
 i like tomcat's logfile style.
 fuck America, Let China Rule The World AGAIN!!!
 '''
-# if floder exists, start logging; else , mkdir logs floder
+# if logs exists, start logging; else ,mkdir logs floder and file
+logfilaname = './logs/' + time.strftime("%Y%m%d") + '.log'
+
 if os.path.exists('./logs'):
-    logfilaname = './logs/' + time.strftime("%Y%m%d") + '.log'
-    with open(logfilaname,'a+') as file:
-        file.close()
-    # if logfilaname size > 200M
-    if os.path.getsize(logfilaname) > 1024**2*200:
+    if os.path.exists(logfilaname):
+         # if logfilaname size > 200M, 1024**2*200
+        if os.path.getsize(logfilaname) > 256:
         # rename logfilaname and delete logfilaname.
-        newlogfilaname = './logs/' + time.strftime("%Y%m%d%H%M%S") + '.log'
-        os.rename(logfilaname,newlogfilaname)
-        os.remove(logfilaname)
+            newlogfilaname = './logs/' + time.strftime("%Y%m%d%H%M%S") + '.log'
+            # rename will delete src file
+            os.rename(logfilaname,newlogfilaname)
+
+        else:
+            pass
     else:
-        pass
+        with open(logfilaname,'a') as file:
+            file.close()
 else:
     os.mkdir('./logs')
-    logfilaname = './logs/' + time.strftime("%Y%m%d") + '.log'
-    
-# basic settings 
+    with open(logfilaname,'a') as file:
+        file.close()
+
+# basic settings
 logging.basicConfig(filename=logfilaname,filemode='a+',level=logging.INFO,encoding='utf-8',format='#### %(asctime)s | %(levelname)s | %(message)s')
 def critical(self):
     return logging.critical(self)
